e360c71e9201d84eb5294e6208bb0cc15cab2a12
zhanglh <zhanglh@asiainfo.com>
2016-03-17 16:25:49 +0800

    修改返回值

--- uac-core/src/main/java/com/ai/opt/uac/api/register/impl/RegisterSVImpl.java
+++ uac-core/src/main/java/com/ai/opt/uac/api/register/impl/RegisterSVImpl.java
@@ -5,12 +5,14 @@ import org.springframework.stereotype.Component;
 
 import com.ai.opt.base.exception.BusinessException;
 import com.ai.opt.base.exception.SystemException;
+import com.ai.opt.base.vo.ResponseHeader;
 import com.ai.opt.sdk.util.BeanUtils;
 import com.ai.opt.sdk.util.DateUtil;
 import com.ai.opt.uac.api.register.interfaces.IRegisterSV;
 import com.ai.opt.uac.api.register.param.phoneRegisterRequest;
 import com.ai.opt.uac.api.register.param.phoneRegisterResponse;
 import com.ai.opt.uac.constants.AccountConstants;
+import com.ai.opt.uac.constants.AccountConstants.ResultCode;
 import com.ai.opt.uac.dao.mapper.bo.GnAccount;
 import com.ai.opt.uac.service.busi.interfaces.IRegisterBusiSV;
 import com.ai.opt.uac.util.VoValidateUtils;
@@ -36,7 +38,9 @@ public class RegisterSVImpl implements IRegisterSV {
         account.setTenantId(AccountConstants.Account.INIT_TENANT_ID);
         long accountId = iRegisterBusiSV.registerByPhone(account);
         phoneRegisterResponse response = new phoneRegisterResponse();
+        ResponseHeader responseHeader = new ResponseHeader(true, ResultCode.SUCCESS_CODE, "成功");
         response.setAccountId(accountId);
+        response.setResponseHeader(responseHeader);
         return response;
     }
 
